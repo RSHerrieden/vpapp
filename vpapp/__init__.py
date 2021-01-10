fd5ba5050ac447a3cfe3c366e35deeacc3c80421
Moritz 'e1mo' Fromm <git@e1mo.de>
2021-01-10 18:08:22 +0100

    Raise version number

--- vpapp/__init__.py
+++ vpapp/__init__.py
@@ -6,6 +6,6 @@ def __is_develop(release):
     return release.endswith("-dev")
 
 name = "vpapp"
-release = "0.1.0"
+release = "0.1.1"
 __version__ = __to_version(release)
 __develop__ = __is_develop(release)
